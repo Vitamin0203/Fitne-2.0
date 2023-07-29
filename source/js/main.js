4708b696a80d8f1d2f9ff157a3137a7d92760443
Vitmin0203 <vitamin.vm0@gmail.com>
2023-07-29 16:47:30 +0300

    Отправляет

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import Accordion from './vendor/accordion';
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -28,7 +29,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 function findVideos() {
-  let videos = document.querySelectorAll('.video');
+  let videos = document.querySelectorAll('.gym-container__video');
 
   for (let i = 0; i < videos.length; i++) {
     setupVideo(videos[i]);
@@ -36,8 +37,8 @@ function findVideos() {
 }
 
 function setupVideo(video) {
-  let link = video.querySelector('.video__link');
-  let button = video.querySelector('.video__button');
+  let link = video.querySelector('.gym-container__video-link');
+  let button = video.querySelector('.gym-container__video-button');
   let id = parseMediaURL(link);
 
   video.addEventListener('click', () => {
@@ -49,7 +50,7 @@ function setupVideo(video) {
   });
 
   link.removeAttribute('href');
-  video.classList.add('video--enabled');
+  video.classList.add('gym-container__video--enabled');
 }
 
 function parseMediaURL(media) {
@@ -66,7 +67,7 @@ function createIframe(id) {
   iframe.setAttribute('allowfullscreen', '');
   iframe.setAttribute('allow', 'autoplay');
   iframe.setAttribute('src', generateURL(id));
-  iframe.classList.add('video__media');
+  iframe.classList.add('gym-container__video-media');
 
   return iframe;
 }
@@ -81,30 +82,30 @@ findVideos();
 
 
 // выбор первого таба
-document.querySelector('.months-list').click();
+document.querySelector('.months-list__item').click();
 
 // скрипт для табов
-const tabsTriggersitems = document.querySelectorAll('.tabs__triggers-item');
+const tabsTriggersitems = document.querySelectorAll('.faq-wrapper__tabs-triggers__item');
 tabsTriggersitems.forEach((item) =>
   item.addEventListener('click', function (e) {
     e.preventDefault();
 
     const id = e.target.getAttribute('href').replace('#', '');
 
-    document.querySelectorAll('.tabs__triggers-item').forEach(
-        (child) => child.classList.remove('tabs__triggers-item--activ')
+    document.querySelectorAll('.faq-wrapper__tabs-triggers__item').forEach(
+        (child) => child.classList.remove('faq-wrapper__tabs-triggers__item--activ')
     );
-    document.querySelectorAll('.tabs__content-item').forEach(
-        (child) => child.classList.remove('tabs__content-item--activ')
+    document.querySelectorAll('.faq-wrapper__tabs-content__item').forEach(
+        (child) => child.classList.remove('faq-wrapper__tabs-content__item--activ')
     );
 
-    item.classList.add('tabs__triggers-item--activ');
-    document.getElementById(id).classList.add('tabs__content-item--activ');
+    item.classList.add('faq-wrapper__tabs-triggers__item--activ');
+    document.getElementById(id).classList.add('faq-wrapper__tabs-content__item--activ');
   })
 
 );
 
-document.querySelector('.tabs__triggers-item').click();
+document.querySelector('.faq-wrapper__tabs-triggers__item').click();
 
 // скрипт на аккордион
 // eslint-disable-next-line no-new, no-undef
@@ -118,39 +119,126 @@ new Accordion('#tab-4');
 
 // swiper
 // eslint-disable-next-line no-new, no-undef
-new Swiper('.swiper', {
-
-  slidesPerView: 4,
-  spaceBetween: 60,
-
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 0,
-    },
-    // when window width is >= 480px
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-    },
-    // when window width is >= 640px
-    1200: {
-      slidesPerView: 4,
-      spaceBetween: 40,
-    },
-  },
-
+let mySwiper = new Swiper('#reviews-slider', {
   direction: 'horizontal',
-  loop: true,
-
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 });
 
+mySwiper.on('slideChange', function () {
+  // Заблокировать кнопку "назад" при первом слайде
+  if (mySwiper.activeIndex === 0) {
+    mySwiper.navigation.$prevEl.addClass('swiper-button-disabled');
+  } else {
+    mySwiper.navigation.$prevEl.removeClass('swiper-button-disabled');
+  }
 
+  // Заблокировать кнопку "вперед" при последнем слайде
+  if (mySwiper.activeIndex === mySwiper.slides.length - 1) {
+    mySwiper.navigation.$nextEl.addClass('swiper-button-disabled');
+  } else {
+    mySwiper.navigation.$nextEl.removeClass('swiper-button-disabled');
+  }
+});
+
+const initTrainersSlider = () => {
+  // eslint-disable-next-line no-unused-vars, no-undef, no-new
+  new Swiper('#trainers-slaider', {
+
+    slidesPerView: 4,
+    spaceBetween: 30,
+
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+      // when window width is >= 480px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      // when window width is >= 640px
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+    },
+
+    direction: 'horizontal',
+    loop: true,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+};
+initTrainersSlider();
+
+// скрипт для валидации
+function validateName(name) {
+  if (name.trim() === '') {
+    return 'Введите имя';
+  }
+  return '';
+}
+
+// Функция для проверки валидности телефона
+function validatePhone(phone) {
+  if (phone.trim() === '') {
+    return 'Введите телефон';
+  }
+  return '';
+}
+
+// Функция для обновления текста ошибки
+function updateErrorLabel(labelElement, errorMessage) {
+  labelElement.textContent = errorMessage;
+  if (errorMessage) {
+    labelElement.style.color = 'red';
+    labelElement.style.marginTop = '34px';
+  } else {
+    labelElement.style.color = '';
+    labelElement.style.marginTop = '';
+  }
+}
+
+// Найти форму
+const form = document.querySelector('.feedback-form__data');
+
+// Найти элементы ввода и метки
+const nameInput = form.querySelector('input[name="name"]');
+const phoneInput = form.querySelector('input[name="phone"]');
+const nameLabel = form.querySelector('.form-name');
+const phoneLabel = form.querySelector('.form-phone');
+
+// Обработчик события отправки формы
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  // Проверка валидности имени и обновление текста ошибки
+  const nameError = validateName(nameInput.value);
+  updateErrorLabel(nameLabel, nameError);
+
+  // Проверка валидности телефона и обновление текста ошибки
+  const phoneError = validatePhone(phoneInput.value);
+  updateErrorLabel(phoneLabel, phoneError);
+
+  // Если нет ошибок, очистить форму
+  if (!nameError && !phoneError) {
+    form.reset();
+  }
+});
+
+// Обработчик события клика по кнопке отправки формы
+const submitButton = form.querySelector('.btn');
+submitButton.addEventListener('click', function () {
+  form.dispatchEvent(new Event('submit'));
+});
 // ---------------------------------
 
 // ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
